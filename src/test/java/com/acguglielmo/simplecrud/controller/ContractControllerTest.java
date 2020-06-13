1c5d24d63856e1070ab2ba4087cb3fd58ce5b5aa
Antonio Guglielmo <antonio_guglielmo@hotmail.com>
2020-06-13 20:19:36 -0300

    Add RestDocs dependency and add documentation to /contracts resource

--- src/test/java/com/acguglielmo/simplecrud/controller/ContractControllerTest.java
+++ src/test/java/com/acguglielmo/simplecrud/controller/ContractControllerTest.java
@@ -1,5 +1,6 @@
 package com.acguglielmo.simplecrud.controller;
 
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -9,8 +10,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.payload.RequestBodySnippet;
+import org.springframework.restdocs.payload.ResponseBodySnippet;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.acguglielmo.simplecrud.request.ContractRequest;
@@ -20,6 +24,7 @@ import br.com.six2six.fixturefactory.Fixture;
 import br.com.six2six.fixturefactory.loader.FixtureFactoryLoader;
 
 @WebMvcTest
+@AutoConfigureRestDocs(outputDir = "target/snippets/contracts")
 public class ContractControllerTest {
 
     private static final String CONTRACTS_BASE_URI = "/contracts";
@@ -47,7 +52,9 @@ public class ContractControllerTest {
         mockMvc.perform( post( CONTRACTS_BASE_URI )
         		.contentType( MediaType.APPLICATION_JSON )
         		.content( mapper.writeValueAsString(request) )
-        	).andExpect(status().isCreated());
+        	)
+        	.andExpect( status().isCreated() )
+        	.andDo( document("POST-201", new RequestBodySnippet() ) );
 
     }
 
@@ -55,15 +62,17 @@ public class ContractControllerTest {
     public void shouldReturnHttp200OkWhenContractIsFoundByIdTest() throws Exception {
 
         mockMvc.perform( get(CONTRACTS_RESOURCE_URI, 1) )
-            .andExpect(status().isOk() );
+            .andExpect(status().isOk() )
+            .andDo(document("GET-by-id-200", new ResponseBodySnippet() ));
 
     }
 
     @Test
-    public void shouldReturnHttp404OkWhenContractIsNotFoundByIdTest() throws Exception {
+    public void shouldReturnHttp404NotFoundWhenContractIsNotFoundByIdTest() throws Exception {
 
         mockMvc.perform( get(CONTRACTS_RESOURCE_URI, 2) )
-            .andExpect(status().isNotFound() );
+            .andExpect(status().isNotFound() )
+            .andDo( document("GET-by-id-404") );
 
     }
 
@@ -75,19 +84,21 @@ public class ContractControllerTest {
         mockMvc.perform( put(CONTRACTS_RESOURCE_URI, 1)
 				.contentType( MediaType.APPLICATION_JSON )
 				.content( mapper.writeValueAsString(request) )
-        	).andExpect(status().isOk());
+        	).andExpect(status().isOk())
+        	.andDo( document("PUT-200") );
 
     }
 
     @Test
-    public void shouldReturnHttp404OkWhenContractIsNotFoundForUpdateTest() throws Exception {
+    public void shouldReturnHttp404NotFoundWhenContractIsNotFoundForUpdateTest() throws Exception {
 
     	final ContractRequest request = Fixture.from(ContractRequest.class).gimme("valid");
 
         mockMvc.perform( put(CONTRACTS_RESOURCE_URI, 2)
     			.contentType( MediaType.APPLICATION_JSON )
     			.content( mapper.writeValueAsString(request) )
-        	).andExpect(status().isNotFound());
+        	).andExpect(status().isNotFound())
+        	.andDo( document("PUT-404") );
 
     }
 
@@ -95,15 +106,17 @@ public class ContractControllerTest {
     public void shouldReturnHttp200OkWhenContractIsDeletedSucessfullyTest() throws Exception {
 
         mockMvc.perform( delete(CONTRACTS_RESOURCE_URI, 1))
-        	.andExpect(status().isOk());
+        	.andExpect(status().isOk())
+        	.andDo( document("DELETE-200") );
 
     }
 
     @Test
-    public void shouldReturnHttp404OkWhenContractIsNotFoundForDeletionTest() throws Exception {
+    public void shouldReturnHttp404NotFoundWhenContractIsNotFoundForDeletionTest() throws Exception {
 
         mockMvc.perform( delete(CONTRACTS_RESOURCE_URI, 2))
-        	.andExpect(status().isNotFound());
+        	.andExpect(status().isNotFound())
+        	.andDo( document("DELETE-404") );
 
     }
 
@@ -111,7 +124,8 @@ public class ContractControllerTest {
     public void shouldReturnHttp200OkWhenNoContractsAreFoundTest() throws Exception {
 
         mockMvc.perform( get(CONTRACTS_BASE_URI) )
-            .andExpect(status().isOk() );
+            .andExpect(status().isOk() )
+            .andDo( document("paginated-GET-200-returning-no-itens") );
 
     }
 
@@ -119,7 +133,8 @@ public class ContractControllerTest {
     public void shouldReturnHttp200OkWhenOneOrMoreContractsAreFoundTest() throws Exception {
 
         mockMvc.perform( get(CONTRACTS_BASE_URI).queryParam("page", "10") )
-            .andExpect(status().isOk() );
+            .andExpect(status().isOk() )
+            .andDo( document("paginated-GET-200-returning-itens", new ResponseBodySnippet() ) );
 
     }
 
